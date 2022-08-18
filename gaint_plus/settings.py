cb193de60166a44aab9142b7d54cc49504eb7347
unknown <kuldeep363rawat@gmail.com>
2022-08-18 13:19:04 +0530

    added env variables

--- gaint_plus/settings.py
+++ gaint_plus/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import os
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 from django.utils.translation import gettext_lazy as _
 
@@ -12,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+sh4+)77v(bv(atm-)=_^4bhu@u%tw!j04#2xs847sz)*l&v+6'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
