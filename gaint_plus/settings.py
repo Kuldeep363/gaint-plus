e364aec5c417e71fe5ed7c7b775fc56fe25bc500
unknown <kuldeep363rawat@gmail.com>
2022-08-18 15:45:16 +0530

    removed .env file

--- gaint_plus/settings.py
+++ gaint_plus/settings.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 import os
-import environ
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
 
 from django.utils.translation import gettext_lazy as _
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -16,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-+sh4+)77v(bv(atm-)=_^4bhu@u%tw!j04#2xs847sz)*l&v+6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
