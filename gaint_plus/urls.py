52a507411e668fd05324f04ca01656171581bb33
unknown <kuldeep363rawat@gmail.com>
2022-08-18 15:17:39 +0530

    changed guide position, changed contact us background gif

--- gaint_plus/urls.py
+++ gaint_plus/urls.py
@@ -11,10 +11,11 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    # prefix_default_language=False,
+    prefix_default_language=True,
 ) +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 #     path('', include('website.urls')),
 #     # prefix_default_language=False,
 #  ] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# 'django-insecure-+sh4+)77v(bv(atm-)=_^4bhu@u%tw!j04#2xs847sz)*l&v+6'
